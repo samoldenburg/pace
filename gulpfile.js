5b85005cd740cfe44e29c5abe35bf15d06a7a9a7
samo@stellarbluetechnologies.com <samo@stellarbluetechnologies.com>
2016-05-08 10:56:02 -0500

    uglify output

--- gulpfile.js
+++ gulpfile.js
@@ -49,6 +49,7 @@ gulp.task("webpack", function(callback) {
         resolve: {
             extensions: ['', '.js', '.jsx']
         },
+        plugins: [new webpack.optimize.UglifyJsPlugin()],
     }, function(err, stats) {
         if(err) throw new gutil.PluginError("webpack", err);
         gutil.log("[webpack]", stats.toString({
